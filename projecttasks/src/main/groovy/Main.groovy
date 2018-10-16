a0690d408226525029e0256e76c9b6c20c7cc4a0
Thad House <thadhouse1@gmail.com>
2018-10-16 09:59:12 -0700

    Fix bug 4 being wrong

--- projecttasks/src/main/groovy/Main.groovy
+++ projecttasks/src/main/groovy/Main.groovy
@@ -1,11 +1,13 @@
 import groovy.transform.CompileStatic
 import org.gradle.api.*
+import org.gradle.api.tasks.TaskContainer
 
 @CompileStatic
 class Main implements Plugin<Project> {
   @Override
   void apply(Project project) {
-    project.tasks.each { Task t ->
+    TaskContainer tasks = project.tasks;
+    tasks.each { Task t->
 
     }
   }
